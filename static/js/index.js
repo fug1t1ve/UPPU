83ef82300bafa8ca56e891eaaf5ae8f264f4ccef
Aniket Maitra <aniketmaitra@am.students.amrita.edu>
2021-09-10 02:32:34 +0530

    version0.1.1

--- static/js/index.js
+++ static/js/index.js
@@ -26,3 +26,38 @@ function loadButtom() {
     $('#downelement').show()
     document.getElementById('downelement').scrollIntoView()
 }
+
+function loadmorecards(){
+    var element = document.getElementById('#cardplace')
+    for(let i = 0;i<2;i++){
+        element.append(`                    <div class="row">
+                        <div class="card mb-3 col-6" style="max-width: 540px;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="{% static 'img/back.png' %}" alt="marijuana" class='card-img-top'>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card mb-3 col-6" style="max-width: 540px;margin-left: auto;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="{% static 'img/back.png' %}" alt="marijuana" class='card-img-top'>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`)
+    }
+}
