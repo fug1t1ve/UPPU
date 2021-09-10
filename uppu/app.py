ddbda02d94bb037a519dd9a1ffcbf1aa063b49b3
Fug171v3 <gbajeli36@gmail.com>
2021-09-10 22:59:02 +0530

    added login form

--- uppu/app.py
+++ uppu/app.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,r'homepage\index.html')
+    return render(request,r'homepage/index.html')
+
+def login(request):
+    return render(request,r'login/login.html')
--- uppu/urls.py
+++ uppu/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .app import index
+from .app import index,login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
+    path('login/',login)
 ]
