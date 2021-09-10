e51fd44bb05c74d002e75ec01a3ec26d979d8864
Aniket Maitra <aniketmaitra@am.students.amrita.edu>
2021-09-11 01:57:04 +0530

    Delete inventory directory

--- inventory/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
-from .views import inventory,addProduct
-
-urlpatterns = [
-    path('',inventory ),
-    path('addProduct/',addProduct),
-]
